c382dc5ce20db44db0484709e070a4ed3ae6dc26
Amarsunkari <95604363+Amarsunkari@users.noreply.github.com>
2021-12-07 16:16:32 +0530

    Add files via upload

---
+++ p or l.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{ 
+  int sp,cp;
+  scanf("%d",&cp);
+  scanf("%d",&sp);
+  if(sp-cp>0)
+  {
+   printf("profit"); 	 
+  }
+  
+  else
+  {
+   printf("loss");
+  }
+ 
+}
