e552b15e3ac88c85d9a6be2c23673799e23c2cb6
Amarsunkari <95604363+Amarsunkari@users.noreply.github.com>
2021-12-07 16:17:09 +0530

    Add files via upload

---
+++ month.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+  int a;
+  scanf("%d",&a);
+  
+  if(0<a<12)
+  {
+    printf("valid month");
+  }
+	 
+  else
+  {
+   printf("not a valid month");	
+   
+   }	 
+	   
+}
